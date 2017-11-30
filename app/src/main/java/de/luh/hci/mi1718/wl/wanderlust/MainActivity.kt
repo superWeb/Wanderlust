0053c8308a61d9a3d1af5a42193da9622f6dabb9
superWeb <microwin2@gmail.com>
2017-11-30 11:47:46 +0100

    twt

---
+++ app/src/main/java/de/luh/hci/mi1718/wl/wanderlust/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.luh.hci.mi1718.wl.wanderlust
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
